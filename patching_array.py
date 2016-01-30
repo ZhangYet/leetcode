752c691763d59c048a6619b7dfc3d80bcb631ae6
Zhang Ye <zhangye@ricebook.com>
2016-01-31 05:39:44 +0800

    not good enough answer.
    This answer for patching arrary problem is not good enough for
    it exceed the memory limit.

--- patching_array.py
+++ patching_array.py
@@ -15,20 +15,28 @@ class Solution(object):
         
         for x in self.nums:
             for i in xrange(0, n):
-                if self.table[i] > 0:
+                if self.table[i] > 0 and self.table[i]+x-1<n:
                     self.table[self.table[i]+x-1] = self.table[i] + x
             self.table[x-1] = x
 
         for i in xrange(1, n+1):
             if i not in self.table:
-                self.refresh(i)
+                self.refresh(i, n)
         return len(self.nums) - self.init_len
             
-    def refresh(self, i):
+    def refresh(self, i, n):
         self.nums.append(i)
 
         for j in xrange(0, n):
-            if self.table[j] > 0:
+            if self.table[j] > 0 and self.table[j]+i-1<n:
                 self.table[self.table[j]+i-1] = self.table[j] + i
         self.table[i-1] = i
         
+        
+def test():
+    nums = [1, 5, 10]
+    n = 20
+    t = Solution()
+    print t.minPatches(nums, n)
+    
+test()
